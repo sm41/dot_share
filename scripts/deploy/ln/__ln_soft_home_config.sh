7d9e2e8e728017c3f0c9bebbcc2fa6df4cf0be5f
sm41 <39440128+sm41@users.noreply.github.com>
2025-07-14 03:54:13 +0900

    add

---
+++ scripts/deploy/ln/__ln_soft_home_config.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -eu
+
+REPOSITORY_DIR=/repository
+DOTFILES_DIR=/dot_share
+ROOT_DIR=/root
+FHS_DIR=/home
+XDG_DIR=/.config
+
+# ${HOME}/.config symbolic link
+while read DOT_CONFIG
+do
+  CONFIG_DIR="${DOT_CONFIG/${REPOSITORY_DIR}${DOTFILES_DIR}${ROOT_DIR}${FHS_DIR}}"
+  mkdir -p "${CONFIG_DIR%/*}"
+
+  if    [[ ! -e "${CONFIG_DIR}"  ]] ; then
+    ln -s "${DOT_CONFIG}"  "${CONFIG_DIR}"
+
+  elif  [[ -L "${CONFIG_DIR}"  ]] ; then
+    ln -s -f "${DOT_CONFIG}"  "${CONFIG_DIR}"
+
+  elif  [[ ! -L "${CONFIG_DIR}"  ]] ; then
+    ln -s -b "${DOT_CONFIG}"  "${CONFIG_DIR}"
+
+  fi
+
+  # echo "${DOT_CONFIG}  ===>   ${CONFIG_DIR}"
+
+done < <( find "${HOME}${REPOSITORY_DIR}${DOTFILES_DIR}${ROOT_DIR}${FHS_DIR}${XDG_DIR}" -not \( -path "*/systemd/user*" \) -type f | sort )
